3ff9a5dd2b43374a6bbeef582b4ce2d8e6bbcbf7
Pablo <189663523+NullPointerHQ@users.noreply.github.com>
2026-09-20 00:48:55 -0400

    Initialization Screen added, user will now see a hamster wheel as the app boots. More Aesthetic work is required before moving to other portions.

--- src/client_app.py
+++ src/client_app.py
@@ -5,8 +5,8 @@ import flet as ft
 import time as time
 from pathlib import Path
 
-# - Startup Screen Flash
-def startup():
+# - App Mascot and Name GUI Elements
+def title_card():
     # - Settings for Mascot's Head and Name
     title_card = ft.Container(
         content= ft.Column(
@@ -30,6 +30,27 @@ def startup():
     cover = ft.Column(controls = [title_card], horizontal_alignment = ft.CrossAxisAlignment.CENTER)
     return cover
 
+def loading():
+    # - Settings for Mascot's Head and Name
+    loading_text = ft.Container(
+        # - Text Settings
+        content = ft.Text(value = "Pulling up your list...", size = 35, italic = True),
+        opacity = 0,
+        animate_opacity = ft.Animation(duration = 800, curve = ft.AnimationCurve.EASE_IN_OUT),
+        )
+
+    # - Loading Wheel Settings        
+    hamster_wheel = ft.Container(content = ft.ProgressRing(width=28, height=28, stroke_width= 3, color= ft.Colors.CYAN), 
+        opacity = 0,
+        animate_opacity = ft.Animation(duration = 800, curve = ft.AnimationCurve.EASE_IN_OUT),
+    )
+
+    # - Combining The text and hamster wheel into one box
+    loading_card = ft.Container(content=ft.Column(controls=[loading_text, hamster_wheel], horizontal_alignment=ft.CrossAxisAlignment.CENTER, spacing=15),
+    margin = ft.Margin(left=0,top=20, right=0, bottom= 20), # - Adjusts the placement of the card (Remember: Controls are inverted)
+    )  
+    return loading_card, loading_text, hamster_wheel
+
 # - Main App Operation 
 def main(page: ft.Page):
 
@@ -51,8 +72,25 @@ def main(page: ft.Page):
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     page.padding = 0
-    page.add(startup()) # - Bringing in the Startup screen
-    time.sleep(5) 
+    
+    page.add(title_card()) # - Bringing in the title card elements
+
+    
+    # - Updating GUI for DB Initialization
+    init_card, init_text, init_hamster_wheel = loading() # - Setting up Loading Screen Objects
+    page.add(init_card) # - Foundation for Load Screen Anims
+    time.sleep(0.3) 
+
+    # - Text Fade In
+    init_text.opacity = 1 
+    page.update()
+    time.sleep(0.8)
+
+    # - Wheel Fade In and Simmed Loading
+    init_hamster_wheel.opacity = 1
+    page.update()
+    time.sleep(3.0)
+
     #page.clean() # - Clears the screen
 
 # - Running the app, please keep at bottom
